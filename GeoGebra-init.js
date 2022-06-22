40dee4cd412fd809e978f3d7debd40a3981eba3d
SusanaMurillo <107961939+SusanaMurillo@users.noreply.github.com>
2022-06-22 15:13:07 +0100

    Create GeoGebra-init.js

---
+++ GeoGebra-init.js
@@ -0,0 +1,42 @@
+let ggbParameters = {
+  id:"ggb",
+  prerelease: false,
+  width: 600,
+  height: 400,
+  showToolBar: false,
+  borderColor: null,
+  showMenuBar: false,
+  showAlgebraInput: false,
+  showResetIcon: true,
+  enableLabelDrags: false,
+  enableShiftDragZoom: true,
+  enableRightClick: true,
+  capturingThreshold: null,
+  showToolBarHelp: false,
+  errorDialogsActive: true,
+  useBrowserForJS: false,
+  material_id: "xgvuxyjc",
+  appletOnLoad:(api)=>ggbConstr.replaceAll("\n","").split(";").forEach((x)=>api.evalCommand(x))
+};
+
+//Iniciar applicação GeoGebra
+//https://wiki.geogebra.org/en/Reference:GeoGebra_Apps_Embedding
+//só no setup() é que inicializa completamente 
+
+let applet = new GGBApplet(ggbParameters, true);
+
+
+//when used with Math Apps Bundle, uncomment this:
+//applet.setHTML5Codebase('GeoGebra/HTML5/5.0/webSimple/');
+
+window.onload = function () {
+
+};
+
+function evalInput(strInput) {
+  ggb.evalCommand(strInput);
+  return false;
+}
+
+
+
